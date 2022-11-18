21649e31c626450e627f8f028248c929dafd2e5b
Shinigou <92162143+Shinigou@users.noreply.github.com>
2022-11-18 01:21:45 +0100

    first upload task6

---
+++ aufg6_typescript/eu.ts
@@ -0,0 +1,28 @@
+/*
+Aufgabe: Aufg 06 EU Data Output
+Datum: 17.11.2022 
+*/
+
+
+//SECTION - Variablen zur Deklaration von Einwohnerzahl 
+
+let germany2008 : number = 82_000_000;
+let germany2022 : number = 84_000_000;
+
+let france2008 : number = 64_300_000;
+let france2022 : number = 67_800_000;
+
+let italia2008 : number = 58_800_000;
+let italia2022 : number = 60_000_000;
+
+let japan2008 : number = 128_000_000;
+let japan2022 : number = 125_700_000;
+
+let all : number = germany2022 + france2022 + italia2022 + japan2022;
+
+
+console.log(
+	`Die Einwohnerzahl in Deutschland beträgt im Jahr 2022: ${germany2022}.` 
+
+
+)
