f906ad13cf7661e22b99e030ef2e1813b6d1873f
Shinigou <92162143+Shinigou@users.noreply.github.com>
2022-11-28 03:34:00 +0100

    update ordnerstruktur

--- Übungen/Testen/leer.js
+++ Übungen/Testen/leer.js
@@ -2,4 +2,4 @@ document.querySelector("button").addEventListener("click", testclick);
 function testclick() {
     console.log("test");
 }
-//# sourceMappingURL=test.js.map
+//# sourceMappingURL=leer.js.map
